dba29330320831b3f607dfbde0ed96cf8b509ded
Saurav26jun <153621889+Saurav26jun@users.noreply.github.com>
2024-03-01 11:58:30 +0530

    Create index.c
    This is the index page of school management system

---
+++ index.c
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>School of Science & Technology</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="icon" type="image/png" href="images/my_favicon new.png">
+</head>
+<body id="my_body">
+    <nav>
+        <span><h1>School of Science & Technology</h1></span>
+        <a href="home.html" id="home">Home</a>
+        <a href="overview.html">Overview</a>
+        <a href="admission.html">Admission</a>
+        <a href="achievement.html">Achievement</a>
+        <a href="facilities.html">Facilities</a>
+        <a href="contact">Contact</a>
+        <a href="about_us.html" id="nav_about_us">About us</a>
+        <a href="support.html">Support</a>
+    </nav>
+
+
+    <main>
+        <table border="1" width="100%" cellpadding="5" cellspacing="5">
+            <tbody>
+                <tr>
+                    <td>
+                        Achievement
+                        <!-- Article no. 1 -->
+                        <div class="article" id="article_1">
+                            <article>
+                                <h4 class="my_achievement">Achievement of 2018</h4>
+                                <time>26 June, 2018</time>
+                                <div>
+                                    <video controls muted width="40%" class="my_video" id="my_video_01">
+                                        <source src="videos/SCHOOL_OF_SCIENCE_&_TECHNOLOGY(1080p).mp4" type="video/mp4">
+                                    </video>
+                                </div>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita excepturi voluptatem, laborum iusto delectus, dignissimos nobis temporibus ducimus nulla velit ratione reiciendis dolorum eos asperiores debitis voluptatibus perferendis cumque quas sequi, exercitationem eius in porro assumenda? Hic aperiam perspiciatis officia, voluptatibus nulla suscipit quasi eligendi et cupiditate expedita quaerat modi nemo minus provident dignissimos illo impedit, ut harum quia qui? Dolor cumque enim corrupti molestiae, saepe dicta deleniti molestias impedit aperiam eum optio eligendi voluptate incidunt minus libero laborum expedita hic cum quidem nisi aut provident qui facere! Accusamus laborum consequatur qui earum atque consequuntur sunt, quam aliquam voluptatibus praesentium, eligendi recusandae veniam, culpa natus! Accusantium, et eius maiores ipsa, earum nostrum dignissimos, quaerat obcaecati dolorum dolor possimus nobis delectus itaque. Vitae laudantium dolore laboriosam ut cumque adipisci quos voluptas aut libero tempora, deserunt exercitationem tempore voluptates explicabo harum culpa ea ratione, fugiat optio accusamus quas illo unde nisi? Nostrum fugit incidunt itaque adipisci ut cupiditate. Maiores fugiat quasi perspiciatis sapiente in aliquid labore asperiores debitis, repellendus id iure incidunt possimus ex minus similique nemo, distinctio fuga consequuntur tenetur fugit eum! Suscipit, expedita maiores! Necessitatibus accusantium sit atque facere assumenda provident excepturi, ducimus minus consequuntur ullam dolore deleniti, porro animi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore aut quaerat vero maxime sit quae ullam minus repellat eaque odio? Ducimus consequuntur odit tenetur? Officia repudiandae eos libero! Fugiat, perferendis.</p>
+                                <a href="achievement.html">Read More</a>
+                           </article>
+                        </div>
+                        <hr />
+                        <!-- Article no. 2 -->
+                        <div class="article" id="article_2">
+                            <article>
+                                <h4 class="my_achievement">Achievement of 2019</h4>
+                                <time>06 Nov, 2019</time>
+                                <div>
+                                    <img src="images/achievement 2019.jpg" alt="achievement 2019" class="my_image" id="my_image_01" width="40%">
+                                </div>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore aut quaerat vero maxime sit quae ullam minus repellat eaque odio? Ducimus consequuntur odit tenetur? Officia repudiandae eos libero! Fugiat, perferendis.</p>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam libero consequuntur voluptatum. Minus, mollitia molestias quod nobis porro ipsum vel eius harum neque? Doloribus, eum eos. Officiis maiores illo officia sint provident enim accusamus temporibus repellendus consequatur nulla? Necessitatibus laudantium, deleniti unde, debitis totam id, accusamus veritatis commodi eius iusto praesentium. Maxime, quam nemo. Deserunt, architecto vel. Repudiandae laudantium esse, aspernatur iste incidunt quam perspiciatis sequi iusto ea dolore modi sit quos aliquid veniam numquam similique quisquam consequatur amet suscipit placeat facilis excepturi nemo autem. Ipsam aperiam ipsa soluta temporibus.</p>
+                                <a href="achievement.html">Read More</a>
+                            </article>
+                        </div>
+                        <hr />
+                        <!-- Article no. 3 -->
+                        <div class="article" id="article_3">
+                            <article>
+                                <h4 class="my_achievement">Achievement of 2021</h4>
+                                <time>21 Aug, 2021</time>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore aut quaerat vero maxime sit quae ullam minus repellat eaque odio? Ducimus consequuntur odit tenetur? Officia repudiandae eos libero! Fugiat, perferendis.</p>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, molestias doloremque non at veniam animi, excepturi, a neque maxime praesentium quos asperiores id dolore error. Sapiente, dolores! Sed quidem, consectetur dolore hic incidunt, adipisci aut quod eaque nisi asperiores amet delectus provident expedita maxime neque, laboriosam nam ab porro! Magnam iusto necessitatibus incidunt in doloremque adipisci voluptates, obcaecati unde a molestiae exercitationem recusandae ipsam, dignissimos minima repellendus voluptatibus quae culpa.</p>
+                                <a href="achievement.html">Read More</a>
+                            </article>
+                        </div>
+                    </td>
+
+                    <td width="25%" valign="top">
+                        <aside>
+                            <form method="GET" action="">
+                                <div class="form">
+                                    <h3 class="form_heading">Subscribe us on social media</h3>
+                                    <label for="f_name" id="f_name">First Name</label><br />
+                                    <input type="text" id="f_name" placeholder="Enter first name" required="required"><br />
+                                    <label for="email">Email</label><br />
+                                    <input type="email" id="email" placeholder="Enter your email" required="required">
+                                    <i class="fa-solid fa-circle-question" title="We don't sell your email, you can trust us"></i><br />
+                                    <button class="button">Subscribe</button>
+                                </div>
+                            </form>
+                    </aside>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
+
+    <table border="2" width="100%" cellpadding="8">
+        <thead>
+            <tr>
+                <th>Admission Management</th>
+                <th>Student Records Management</th>
+                <th>Attendance Management</th>
+                <th>Fees Management</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>                
+                    <ul style="list-style-type:disc">
+                        <li>Easy Generation of Merit Lists</li>
+                        <li>Mobile Application Integration</li>
+                        <li>Admission Status - Live Tracking</li>
+                        <li>Online Fee Payment Gateway</li>
+                        <li>Real-Time Reports</li>
+                    </ul>                
+                </td>
+                <td>                    
+                    <ul style="list-style-type:circle">
+                        <li>Easy Data Search & retrieval</li>
+                        <li>Role-based Access</li>
+                        <li>Centralized System For Managing Records</li>
+                        <li>Student Performance Evalution</li>
+                        <li>Direct Communication among Departments</li>
+                    </ul>
+                </td>
+                <td>                 
+                    <ul style="list-style-type:disc">
+                        <li>Biometric & RFID Device Integrations</li>
+                        <li>Accurate Students Attendance Records</li>
+                        <li>Auto-Generated Email/SMS Notification</li>
+                        <li>Monitoring Absentees & Leave</li>
+                        <li>Easy Attendance Report Generation</li>
+                    </ul>
+                </td>
+                <td>    
+                    <ul style="list-style-type:circle">
+                        <li>Payment Gateways - UPI, Credit/Debit card</li>
+                        <li>Accurate fees reports</li>
+                        <li>100% Safe Transaction</li>
+                        <li>Auto Notification for Pending Fees</li>
+                        <li>AI-Powered Analytics Dashboard</li>
+                    </ul>
+                </td>
+            </tr>
+        </tbody>    
+    </table>
+
+    <footer>
+
+        <p class="my_socialmedia">Join us on social media</p>
+
+        <div>
+            <a href="https://www.facebook.com/profile.php?id=100054553220287&mibextid=2JQ9oc" class="fb_class"><i class="fa-brands fa-facebook" id="my_facebook_account" title="facebook"></i></a>
+
+            <a href="https://www.youtube.com/@schoolofsciencetechnologyc854/about"><i class="fa-brands fa-youtube" id="my_youtube_account" title="youtube"></i></a>
+            
+            <a href="https://www.linkedin.com/in/saurav-raj-yadav-89a737208"><i class="fa-brands fa-linkedin" id="my_linkedin_account" title="linkedin"></i></a>
+        
+        </div>
+        <p>
+            &copy; 2030, sstislampur.com
+        </p>
+        <i class="fa-solid fa-book-open"></i>
+    </footer>
+
+</body>
+</html>
